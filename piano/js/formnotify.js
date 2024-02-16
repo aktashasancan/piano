c612e9badffbfddf64549740ec06faeeb657c67e
hasancan <aktashasan634@gmail.com>
2024-02-16 12:53:54 +0300

    piano
    web sitesi

---
+++ piano/js/formnotify.js
@@ -0,0 +1,30 @@
+﻿if (varmierror == '0')
+
+{
+$(document).ready(function () {
+$.notify({
+// options
+icon: 'glyphicon glyphicon-thumbs-up',
+title: 'MESAJINIZ BAŞARIYLA BİZE ULAŞTI!',
+message: 'Teşekkürler, ekibimiz en kısa sürede ilettiğiniz konuyla ilgili size geri dönüş sağlayacaktır.'
+}, {
+// settings
+type: 'success'
+});
+    });
+}
+
+if (varmierror == '1')
+        {
+            $(document).ready(function () {
+                $.notify({
+                    // options
+                    icon: 'glyphicon glyphicon-thumbs-up',
+                    title: 'MESAJ ULAŞTIRILAMADI!',
+                    message: 'Lütfen form doğrulama sorusunun cevabını doğru yazdığınızdan emin olarak tekrar deneyiniz.'
+                }, {
+                    // settings
+                    type: 'danger'
+                });
+            });
+        }
